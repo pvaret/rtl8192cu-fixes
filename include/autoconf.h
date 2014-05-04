cf898ff6ad3f764d8ebe367088f39a03ee3a76ff
Paolo D'Apice <paolo.dapice@gmail.com>
2014-05-04 11:25:29 +0800

    Disabled debug messages

--- include/autoconf.h
+++ include/autoconf.h
@@ -296,9 +296,9 @@
 //#define CONFIG_DEBUG_RTL871X
 
 #define DBG	0
-#define CONFIG_DEBUG_RTL819X
+//#define CONFIG_DEBUG_RTL819X
 
-#define CONFIG_PROC_DEBUG	1
+//#define CONFIG_PROC_DEBUG	1
 
 //#define DBG_IO
 //#define DBG_DELAY_OS
